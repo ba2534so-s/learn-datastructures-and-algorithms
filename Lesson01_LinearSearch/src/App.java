6297b26d851a1b48134c712376f794411c7eab09
Baran <Baran.soysuren@gmail.com>
2024-08-30 23:22:25 +0200

    add sorted array

--- Lesson01_LinearSearch/src/App.java
+++ Lesson01_LinearSearch/src/App.java
@@ -1,5 +1,7 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        
+        int[] sortedArray = {4, 7, 9, 14, 19};
+
     }
 }
