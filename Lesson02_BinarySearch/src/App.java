a2d60609076db01ff088a073dc08cbddae26bbc3
Baran <Baran.soysuren@gmail.com>
2024-08-31 22:20:32 +0200

    create binarySearch method signature

--- Lesson02_BinarySearch/src/App.java
+++ Lesson02_BinarySearch/src/App.java
@@ -2,4 +2,9 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+
+
+    public static int binarySearch(int[] sortedNumbers, int targetNumber) {
+        
+    }
 }
