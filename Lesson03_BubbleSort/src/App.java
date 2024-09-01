4819abbdfc752ca8aa10108484d1eeb86d649cb8
Baran <Baran.soysuren@gmail.com>
2024-09-02 00:03:51 +0200

    add enhanced for loops for printing out numbers before and after sorting

--- Lesson03_BubbleSort/src/App.java
+++ Lesson03_BubbleSort/src/App.java
@@ -3,5 +3,16 @@ public class App {
         
         int[] numbers = {6, 5, 2, 8, 9, 4};
 
+        for (int n : numbers) {
+            System.out.print(n);
+        }
+        System.out.println();
+
+
+        for (int n : numbers) {
+            System.out.print(n);
+        }
+        System.out.println();
+
     }
 }
