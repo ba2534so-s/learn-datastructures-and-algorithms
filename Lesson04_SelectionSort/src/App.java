1e07cc606ebe6d54b4b397229ed9548443cd2c7c
Baran <Baran.soysuren@gmail.com>
2024-09-02 01:18:45 +0200

    add main code

--- Lesson04_SelectionSort/src/App.java
+++ Lesson04_SelectionSort/src/App.java
@@ -2,6 +2,18 @@ public class App {
     public static void main(String[] args) throws Exception {
         int[] numbers = {6, 5, 2, 8, 9, 4, 1};
 
-        
+        System.out.print("Before Sorting: ");
+        for (int n : numbers) {
+            System.out.print(n + " ");
+        }
+        System.out.println();
+
+        selectionSort(numbers);
+
+        System.out.print("After Sorting: ");
+        for (int n : numbers) {
+            System.out.print(n + " ");
+        }
+        System.out.println();
     }
 }
