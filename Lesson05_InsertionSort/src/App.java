7fc894cea0d9cd1dde67c48d9346ffcfef2814c7
Baran <Baran.soysuren@gmail.com>
2024-09-02 02:18:03 +0200

    create array of unsorted numbers

--- Lesson05_InsertionSort/src/App.java
+++ Lesson05_InsertionSort/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int[] numbers = {6, 5, 2, 8, 9, 4, 1};
+
+        
     }
 }
