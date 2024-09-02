b90e4161e44d93a9c4552048537c2e77660ffb1a
Baran <Baran.soysuren@gmail.com>
2024-09-02 02:18:37 +0200

    add before and after loops to iterate over array

--- Lesson05_InsertionSort/src/App.java
+++ Lesson05_InsertionSort/src/App.java
@@ -2,6 +2,17 @@ public class App {
     public static void main(String[] args) throws Exception {
         int[] numbers = {6, 5, 2, 8, 9, 4, 1};
 
-        
+        System.out.print("Before Sorting: ");
+        for (int n : numbers) {
+            System.out.print(n + " ");
+        }
+        System.out.println();
+
+
+        System.out.print("After Sorting: ");
+        for (int n : numbers) {
+            System.out.print(n + " ");
+        }
+        System.out.println();
     }
 }
