5ef53ef90d7508782a2e1ee117374e31520b32a8
Baran <Baran.soysuren@gmail.com>
2024-09-02 03:13:41 +0200

    add unsorted array

--- Lesson06_QuickSort/src/App.java
+++ Lesson06_QuickSort/src/App.java
@@ -1,5 +1,5 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        int[] numbers = {6, 5, 2, 8, 9, 4, 1};
     }
 }
