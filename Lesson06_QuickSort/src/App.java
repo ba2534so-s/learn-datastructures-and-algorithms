1c71ebb642ab0e3e1d4b83085e9620e7b4fe1912
Baran <Baran.soysuren@gmail.com>
2024-09-02 03:14:42 +0200

    print out array before and after sorting

--- Lesson06_QuickSort/src/App.java
+++ Lesson06_QuickSort/src/App.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) {
         int[] numbers = {6, 5, 2, 8, 9, 4, 1};
+
+        System.out.print("Before Sorting: ");
+        for (int n : numbers) {
+            System.out.print(n + " ");
+        }
+        System.out.println();
+
+
+        System.out.print("After Sorting: ");
+        for (int n : numbers) {
+            System.out.print(n + " ");
+        }
+        System.out.println();
     }
 }
