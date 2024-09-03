894be943dd5bfea96232d1006bb45a849e8d6896
Baran <Baran.soysuren@gmail.com>
2024-09-03 06:18:09 +0200

    insert three values

--- Lesson08_LinkedList/src/com/learndsa/lesson8/Main.java
+++ Lesson08_LinkedList/src/com/learndsa/lesson8/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
 
         LinkedList list = new LinkedList();
-        
+        list.insert(18);
+        list.insert(52);
+        list.insert(36);
     }
 }
