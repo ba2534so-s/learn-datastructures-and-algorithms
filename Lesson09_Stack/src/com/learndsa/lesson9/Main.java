05d52a59e57c648b46cd4be2a592713f17aa88dd
Baran <Baran.soysuren@gmail.com>
2024-09-07 01:24:52 +0200

    change name of stack to StaticStack

--- Lesson09_Stack/src/com/learndsa/lesson9/Main.java
+++ Lesson09_Stack/src/com/learndsa/lesson9/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         
-        Stack stack = new Stack();
+        StaticStack stack = new StaticStack();
 
         System.out.println("Empty: " + stack.isEmpty());
 
