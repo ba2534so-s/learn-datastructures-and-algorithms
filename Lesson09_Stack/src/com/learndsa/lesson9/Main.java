32b887f493d3c65b16cf357ff92aa3e556a945f4
Baran <Baran.soysuren@gmail.com>
2024-09-07 01:02:38 +0200

    call isEmpty before adding values

--- Lesson09_Stack/src/com/learndsa/lesson9/Main.java
+++ Lesson09_Stack/src/com/learndsa/lesson9/Main.java
@@ -3,7 +3,11 @@ package com.learndsa.lesson9;
 public class Main {
     public static void main(String[] args) {
 
+        
         Stack stack = new Stack();
+
+        System.out.println("Empty: " + stack.isEmpty());
+
         stack.push(18);
         stack.push(5);
         stack.push(13);
