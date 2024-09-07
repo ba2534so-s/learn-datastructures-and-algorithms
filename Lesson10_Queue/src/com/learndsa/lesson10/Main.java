5b3b7fa31912f959d8178fe36a641ff10108e520
Baran <Baran.soysuren@gmail.com>
2024-09-07 03:06:46 +0200

    call enQueue

--- Lesson10_Queue/src/com/learndsa/lesson10/Main.java
+++ Lesson10_Queue/src/com/learndsa/lesson10/Main.java
@@ -3,5 +3,7 @@ package com.learndsa.lesson10;
 public class Main {
     public static void main(String[] args) {
         StaticQueue sQueue = new StaticQueue();
+        
+        sQueue.enQueue(34);
     }
 }
