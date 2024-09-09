d53ff19a795cbdd8cf2e8dce1304eb56684902a8
Baran <Baran.soysuren@gmail.com>
2024-09-09 03:13:39 +0200

    create instance of BinaryTree

--- Lesson11_BinaryTree/src/com/learndsa/lesson11/Main.java
+++ Lesson11_BinaryTree/src/com/learndsa/lesson11/Main.java
@@ -2,6 +2,6 @@ package com.learndsa.lesson11;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        BinaryTree tree = new BinaryTree();
     }
 }
