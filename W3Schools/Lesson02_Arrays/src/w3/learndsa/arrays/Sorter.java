022b2d2476fa07ccafd3226d26cddc0be3e1bdb5
Baran <Baran.soysuren@gmail.com>
2024-11-17 00:37:17 +0100

    create inner loop

--- W3Schools/Lesson02_Arrays/src/w3/learndsa/arrays/Sorter.java
+++ W3Schools/Lesson02_Arrays/src/w3/learndsa/arrays/Sorter.java
@@ -5,6 +5,9 @@ public class Sorter {
 	public int[] bubbleSort(int[] intArray) {
 		for (int i = 0; i < intArray.length; i++) {
 			boolean swapped = false;
+			for (int j = 0; j < intArray.length-i-1; j++) {
+				
+			}
 		}
 	}
 
