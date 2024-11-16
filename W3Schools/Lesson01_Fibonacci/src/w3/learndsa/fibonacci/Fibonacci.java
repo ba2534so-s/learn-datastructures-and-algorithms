dba68e106ca06c87eab4d859560732336c450314
Baran <Baran.soysuren@gmail.com>
2024-11-16 23:19:06 +0100

    add fibonacci for loop method signature

---
+++ W3Schools/Lesson01_Fibonacci/src/w3/learndsa/fibonacci/Fibonacci.java
@@ -0,0 +1,17 @@
+package w3.learndsa.fibonacci;
+
+public class Fibonacci {
+	
+	// 1. Start with the two Fibonacci numbers 0 and 1
+	// 		a. Add the two previous numbers together to create a new Fibonacci number
+	// 		b. Update the values of the previous two numbers
+	// 2. Do point a and b 18 times to get the 20th Fibonacci
+	
+	public void fibonacciForLoop() {
+		
+		
+	}
+	
+	
+
+}
