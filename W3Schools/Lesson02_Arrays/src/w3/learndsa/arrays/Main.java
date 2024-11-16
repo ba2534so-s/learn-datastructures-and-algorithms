cda11dd329db8b4aa7151ddae989ff6589cd2d85
Baran <Baran.soysuren@gmail.com>
2024-11-17 00:43:39 +0100

    print unsorted array

--- W3Schools/Lesson02_Arrays/src/w3/learndsa/arrays/Main.java
+++ W3Schools/Lesson02_Arrays/src/w3/learndsa/arrays/Main.java
@@ -6,7 +6,11 @@ public class Main {
 		// TODO Auto-generated method stub
 		int[] intArray = {7, 12, 9, 3, 8, 5, 2, 10, 9, 11};
 		
-		
+		System.out.print("Unsorted Array: ");
+		for(int i : intArray) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
 
 	}
 
