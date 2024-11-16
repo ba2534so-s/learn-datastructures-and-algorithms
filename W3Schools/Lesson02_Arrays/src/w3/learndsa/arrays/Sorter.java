00beb364c24293b3ccd43d4fd5a4d1d556baec59
Baran <Baran.soysuren@gmail.com>
2024-11-17 00:40:09 +0100

    swap values

--- W3Schools/Lesson02_Arrays/src/w3/learndsa/arrays/Sorter.java
+++ W3Schools/Lesson02_Arrays/src/w3/learndsa/arrays/Sorter.java
@@ -7,7 +7,9 @@ public class Sorter {
 			boolean swapped = false;
 			for (int j = 0; j < intArray.length-i-1; j++) {
 				if (intArray[j] > intArray[j+1]) {
-					
+					int temp = intArray[j];
+					intArray[j] = intArray[j+1];
+					intArray[j+1] = temp;
 				}
 			}
 		}
